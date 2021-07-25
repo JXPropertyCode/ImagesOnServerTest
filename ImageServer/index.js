bee1c6ed8f90ab018803d64ec2b0bab3d235bead
JXPropertyCode <yellowdice92@hotmail.com>
2021-07-24 18:17:44 -0700

    added comments

---
+++ ImageServer/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const p = 3000;
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors("*"));
+
+// makes static is a middleware
+// exposes my public folder so people can use it
+// the first parameter means you need to use /public
+// ex. http://localhost:3000/public/images/modelS.jpg
+// refer to https://expressjs.com/en/starter/static-files.html
+app.use('/public', express.static("public"));
+
+// custom middleware to intercept
+app.use( (req, res, next) =>  {
+	console.log("hi")
+	next()
+})
+
+app.get("/", function (req, res) {
+	res.send("expressPicture");
+});
+
+app.get("/random", function (req, res) {
+	res.send("random path");
+});
+
+app.listen(p, function () {
+	console.log(`Server started on localhost:${p}`);
+});
